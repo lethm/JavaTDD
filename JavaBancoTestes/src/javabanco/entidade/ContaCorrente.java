a66936dc88b21197ae5c815d80d7345ed165fb20
Leticia Hass <lethassm@gmail.com>
2017-07-04 10:43:07 -0300

    Foi criado transferência, classe operação e implementado os testes

--- JavaBancoTestes/src/javabanco/entidade/ContaCorrente.java
+++ JavaBancoTestes/src/javabanco/entidade/ContaCorrente.java
@@ -1,10 +1,18 @@
 package javabanco.entidade;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ContaCorrente {
+	private String _titular;
+	private int _numero;
 	private float _saldo = 0;
-	private ArrayList<Float> _operacoes = new ArrayList<Float>();
+	private ArrayList<Operacao> _operacoes = new ArrayList<Operacao>();
+	public ContaCorrente (int numeroConta, String titularConta){
+		_numero = numeroConta;
+		_titular = titularConta;
+	}
+	
 	public float saldo(){
 		return _saldo;
 	}
@@ -12,18 +20,34 @@ public class ContaCorrente {
 	public float credito(float valor) {
 		if (valor <= 0) throw new IllegalArgumentException("O valor da operacao deve ser maior que zero");
 		_saldo += valor;
-		_operacoes.add(valor);
+		Operacao op = new Operacao (valor, "CREDITO", new Date());
+		_operacoes.add(op);
 		return _saldo;
 	}
 	
 	public float debito(float valor) {
 		if (valor <= 0) throw new IllegalArgumentException("O valor da operacao deve ser maior que zero");
 		_saldo -= valor;
-		_operacoes.add(-valor);
+		Operacao op = new Operacao(valor, "DEBITO", new Date());
+		_operacoes.add(op);
 		return _saldo;
 	}
 	
-	public ArrayList<Float> extrato() {
+	public ArrayList<Operacao> extrato(int i, String string) {
 		return _operacoes;
+	}
+	
+	public String getTitular(){
+		return _titular;
+	}
+	
+	public int getNumero(){
+		return _numero;
+	}
+	
+	public float transfência(float valor, ContaCorrente ccDestino) {
+		this.debito(valor);
+		ccDestino.credito(valor);
+		return saldo();
 	}
 }
--- JavaBancoTestes/src/javabanco/entidade/ContaCorrenteTest.java
+++ JavaBancoTestes/src/javabanco/entidade/ContaCorrenteTest.java
@@ -1,6 +1,6 @@
 package javabanco.entidade;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
@@ -10,38 +10,40 @@ public class ContaCorrenteTest {
 
 	@Test
 	public void testSaldo() {
-		ContaCorrente cc = new ContaCorrente();
+		ContaCorrente cc = new ContaCorrente(12345, "Joao da Silva");
 		assertEquals(0, cc.saldo(), 0);
 	}
 
 	@Test
 	public void testCredito() {
-		ContaCorrente cc = new ContaCorrente();
+		ContaCorrente cc = new ContaCorrente(12345, "Joao da Silva");
 		cc.credito(100);
 		assertEquals(100, cc.saldo(), 0);
 	}
 
 	@Test
 	public void testDebito() {
-		ContaCorrente cc = new ContaCorrente();
+		ContaCorrente cc = new ContaCorrente(12345, "Joao da Silva");
 		cc.debito(100);
 		assertEquals(-100, cc.saldo(), 0);
 	}
 
 	@Test
 	public void testExtrato() {
-		ContaCorrente cc = new ContaCorrente();
+		ContaCorrente cc = new ContaCorrente(12345, "Joao da Silva");
 		cc.credito(100);
 		cc.debito(100);
-		ArrayList<Float> op = cc.extrato();
+		ArrayList<Operacao> op = (ArrayList<Operacao>) cc.extrato(0, null);
 		assertEquals(2, op.size());
-		assertEquals(100, op.get(0), 0);
-		assertEquals(-100, op.get(1), 0);
+		assertEquals(100, op.get(0).getValor(), 0);
+		assertEquals("CREDITO", op.get(0).getTipoOperacao());
+		assertEquals(100, op.get(1).getValor(),0);
+		assertEquals("DEBITO", op.get(1).getTipoOperacao());
 	}
 	
 	@Test
 	public void testSaldoPequenosFloats() {
-		ContaCorrente cc = new ContaCorrente();
+		ContaCorrente cc = new ContaCorrente(12345, "Joao da Silva");
 		cc.credito(0.1f);
 		cc.credito(0.2f);
 		assertEquals(0.3f, cc.saldo(), 0.0f);
@@ -49,16 +51,44 @@ public class ContaCorrenteTest {
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testCreditoValorNegativo() {
-		ContaCorrente cc = new ContaCorrente();
+		ContaCorrente cc = new ContaCorrente(12345, "Joao da Silva");
 		
 		cc.credito(-10);		
+	}
+	@Test
+	public void testConstrutor(){
+		ContaCorrente cc = new ContaCorrente(12345, "João da Silva");
+		assertNotNull(cc);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testDebitoValorNegativo() {
-		ContaCorrente cc = new ContaCorrente();
+		ContaCorrente cc = new ContaCorrente(12345, "Joao da Silva");
 		
 		cc.debito(-10);		
 	}
-
+	
+	@Test
+	public void testGetTitular(){
+		ContaCorrente cc = new ContaCorrente(12345, "Joao da Silva");
+		assertEquals("Joao da Silva", cc.getTitular());
+	}
+	
+	@Test
+	public void testGetNumero(){
+		ContaCorrente cc = new ContaCorrente(12345, "Joao da Silva");
+		assertEquals(12345, cc.getNumero());
+	}
+	
+	@Test
+	public void Transferencial(){
+		ContaCorrente ccOrigem = new ContaCorrente(12345, "Joao da Silva");
+		ContaCorrente ccDestino = new ContaCorrente(12345, "Joao da Silva");
+		//transfere
+		ccOrigem.transfência(100, ccDestino);
+		//testa
+		assertEquals(-100, ccOrigem.saldo(),0);
+		assertEquals(100, ccDestino.saldo(),0);
+	}
+ 
 }
